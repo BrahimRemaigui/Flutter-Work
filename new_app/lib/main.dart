39ab8285c2b1ea07f5d0533e082d396b441e5ed9
PC-IT <ibrahimremaigui@gmail.com>
2023-03-13 23:23:55 +0100

    update

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -8,15 +8,42 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
-            title: Text("the first app with flutter"),
-          ),
-          body: Row(
-            children: <Widget>[
-              Text("ehh  "),
-              Text("shfskfj"),
+            backgroundColor: Colors.teal,
+            leading: Icon(
+              Icons.access_time,
+              color: Colors.black,
+            ),
+            title: Text(
+              "The first app with flutter",
+            ),
+            actions: const [
+              Center(child: Text('Homr  ')),
+              Center(child: Text('Add Product  ')),
+              Center(child: Text('Consult The Syock  ')),
             ],
+          ),
+          body: Container(
+            color: Colors.blueGrey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "heloo",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: AutofillHints.birthday,
+                      fontWeight: FontWeight.w600),
+                ),
+                Text(' word'),
+                Text('yyyy'),
+                Text('heloo'),
+                Text(' word'),
+                Text('yyyy'),
+              ],
+            ),
           )),
     );
   }
